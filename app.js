6bcb4fbc6296a7fea8f315db1eafef0cae5392d8
jdamilasp <jdamilasp@gmail.com>
2019-12-24 15:01:42 +0800

    add ping end point result as pong

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/ping', (req,res) => {
+  return res.send('pong')
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
