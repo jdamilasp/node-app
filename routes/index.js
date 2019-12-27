c8c56600ec35cd93971aaaddeb57df1ccd8448de
jdamilasp <jdamilasp@gmail.com>
2019-12-27 14:14:49 +0800

    add post method to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  
+  const resBody  = { method : 'GET', url : '/' }
+  console.log(resBody)
+  return res.status(200).json(resBody)
+
+});
+
+router.post('/', (req, res, next) => {
+  
+  const resBody  = { method : 'GET', url : '/', body: req.body }
+  console.log(resBody)
+  return res.status(200).json(resBody)
+  
 });
 
 module.exports = router;
